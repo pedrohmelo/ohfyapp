bc948f07ff566519dee54db87510aa2ad0ad4a0b
pedrohmelo <phmc_1995@hotmail.com>
2022-08-02 11:18:54 -0300

    inicio carrossel

--- src/screen/Home/Home.index.tsx
+++ src/screen/Home/Home.index.tsx
@@ -1,18 +1,14 @@
 import React from "react"
 import {
     Text,
-    View
+    View,
+    SafeAreaView
 } from "react-native"
-import { styles } from "../SignIn/SignIn.styles"
+import { styles } from "../Home/Home.styles"
+import Teste from "../../components/carrossel/Carrossel.index"
 
 export function Home(){
     return(
-        <View style={styles.container}>
-            <View>
-                
-            </View>
-            <Text>AAAAA</Text>
-        </View>
-
+        <Teste/>
     )
 }
--- src/screen/Home/Home.styles.ts
+++ src/screen/Home/Home.styles.ts
@@ -2,6 +2,12 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1
+        backgroundColor: 'green'
+    },
+    backgroundImg: {
+
+    },
+    carroussel: {
+
     },
 })
